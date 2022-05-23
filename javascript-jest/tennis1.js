91fdee98b4cf02e1402f178b4d870358331f7db7
Albert Mañosa <albertmasa2@gmail.com>
2022-05-23 13:05:38 +0200

    extract `lateGameScore` function

--- javascript-jest/tennis1.js
+++ javascript-jest/tennis1.js
@@ -6,11 +6,7 @@ function getScore(m_score1, m_score2) {
     if (m_score1 === m_score2) {
         score = equalScore(m_score1);
     } else if (m_score1 >= 4 || m_score2 >= 4) {
-        var minusResult = m_score1 - m_score2;
-        if (minusResult === 1) {score = "Advantage player1";}
-        else if (minusResult === -1) {score = "Advantage player2";}
-        else if (minusResult >= 2) {score = "Win for player1";}
-        else {score = "Win for player2";}
+        score = lateGameScore(m_score1, m_score2);
     } else {
         for (var i = 1; i < 3; i++) {
             if (i === 1) {tempScore = m_score1;}
@@ -35,6 +31,14 @@ function getScore(m_score1, m_score2) {
         }
     }
     return score;
+}
+
+function lateGameScore(m_score1, m_score2) {
+    var minusResult = m_score1 - m_score2;
+    if (minusResult === 1) { return "Advantage player1"; }
+    else if (minusResult === -1) { return "Advantage player2"; }
+    else if (minusResult >= 2) { return "Win for player1"; }
+    else { return "Win for player2"; }
 }
 
 function equalScore(score) {
