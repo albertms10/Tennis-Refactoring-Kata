e2c6dc1e816e851e3269e373ac4c19e57a35179f
Albert Mañosa <albertmasa2@gmail.com>
2022-05-23 13:17:58 +0200

    remove `for` statement

--- javascript-jest/tennis1.js
+++ javascript-jest/tennis1.js
@@ -14,27 +14,47 @@ function getScore(m_score1, m_score2) {
 
 function unequalOrNonLateGameScore(m_score1, score, m_score2) {
     var tempScore = 0;
-    for (var i = 1; i < 3; i++) {
-        if (i === 1) { tempScore = m_score1; }
-        else {
-            score += "-";
-            tempScore = m_score2;
-        }
-        switch (tempScore) {
-            case 0:
-                score += "Love";
-                break;
-            case 1:
-                score += "Fifteen";
-                break;
-            case 2:
-                score += "Thirty";
-                break;
-            case 3:
-                score += "Forty";
-                break;
-        }
+    var i = 1;
+    if (i === 1) { tempScore = m_score1; }
+    else {
+        score += "-";
+        tempScore = m_score2;
     }
+    switch (tempScore) {
+        case 0:
+            score += "Love";
+            break;
+        case 1:
+            score += "Fifteen";
+            break;
+        case 2:
+            score += "Thirty";
+            break;
+        case 3:
+            score += "Forty";
+            break;
+    }
+    i = 2;
+    if (i === 1) { tempScore = m_score1; }
+    else {
+        score += "-";
+        tempScore = m_score2;
+    }
+    switch (tempScore) {
+        case 0:
+            score += "Love";
+            break;
+        case 1:
+            score += "Fifteen";
+            break;
+        case 2:
+            score += "Thirty";
+            break;
+        case 3:
+            score += "Forty";
+            break;
+    }
+    
     return score;
 }
 
